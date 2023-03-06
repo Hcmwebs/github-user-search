29fb74294e7fda97007116c0a6f02e7757b6c01d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-06 21:37:23 +1300

    created and indexed the compos
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = () => {
+	return (
+		<form>
+			<input type='text' />
+			<button className='search'>Search</button>
+		</form>
+	)
+}
+
+export default Search
