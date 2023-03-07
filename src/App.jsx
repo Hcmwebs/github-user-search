86b7e31fa68889eec82c96b654d0dfd8129ca7c2
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-08 11:06:36 +1300

    added landmarks to the app and the Wrapper
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import { Info, Navbar, Search } from './components'
 import { GlobalStyles } from './styles'
+import styled from 'styled-components'
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<div className='App'>
-        <Navbar />
-        <Search />
-        <Info />
-			</div>
+			<Wrapper className='App'>
+				<header className='sectionCenter' >
+					<Navbar />
+				</header>
+				<main className='sectionCenter'>
+					<Search />
+					<Info />
+				</main>
+			</Wrapper>
 		</>
 	)
 }
+
+const Wrapper = styled.div`
+	&.App {
+		width: 375px;
+		outline: 1px solid white;
+		display: grid;
+		place-items: center;
+		gap: 2.25rem;
+		@media (min-width: 768px) {
+			width: 573px;
+			gap: 2.8rem;
+		}
+		@media (min-width: 1440px) {
+			width: 730px;
+			gap: 2.25rem;
+		}
+	}
+`
 
 export default App
