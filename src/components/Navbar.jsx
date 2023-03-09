9d4b572398c79d4e5b4f52e1669613f23427a38d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-09 16:23:34 +1300

    updated and styled the content
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import sun from '../assets/icon-sun.svg'
 const Navbar = () => {
 	return (
-		<>
-			<div>Logo</div>
-			<button className='toggleBtn'>toggle</button>
-		</>
+		<Wrapper className='sectionCenter'>
+			<a href='#' className='logo'>
+			devfinder
+			</a>
+			<button className='toggleBtn'>
+				light
+				<img src={sun} alt='light icon' />
+			</button>
+		</Wrapper>
 	)
 }
+
+const Wrapper = styled.nav`
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	.logo {
+		color: white;
+	}
+
+	.toggleBtn {
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+		gap: 1rem;
+		text-transform: uppercase;
+	}
+`
 
 export default Navbar
