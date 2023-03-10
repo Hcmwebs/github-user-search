e861ff1885153638af95c774a14a4f5a6b3edb48
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-10 14:41:38 +1300

    set up theme buttons dynamically
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import sun from '../assets/icon-sun.svg'
-const Navbar = () => {
+import moon from '../assets/icon-moon.svg'
+import { LightBtn, DarkBtn } from './index'
+
+const Navbar = ({ toggleTheme, theme }) => {
 	return (
 		<Wrapper className='sectionCenter'>
 			<a href='#' className='logo'>
 				devfinder
 			</a>
-			<button className='toggleBtn'>
-				<small>light</small>
-				<img src={sun} alt='light icon' />
-			</button>
+			{theme === 'lightTheme' ? (
+				<DarkBtn toggleTheme={toggleTheme} />
+			) : (
+				<LightBtn toggleTheme={toggleTheme} />
+			)}
 		</Wrapper>
 	)
 }
@@ -25,7 +29,7 @@ const Wrapper = styled.nav`
 		font-size: 1.625rem;
 		line-height: 2.4375rem;
 		font-weight: var(--bold);
-		color: white;
+		color: var(--logoColor);
 	}
 
 	.toggleBtn {
@@ -35,17 +39,24 @@ const Wrapper = styled.nav`
 		align-items: center;
 		gap: 1rem;
 		text-transform: uppercase;
-		background-color:transparent;
-
-		small {
-			font-size: 0.8125rem;
-			font-weight: var(--bold);
-			line-height: 1.1875rem;
+		background-color: transparent;
+		font-size: 0.8125rem;
+		font-weight: var(--bold);
+		line-height: 1.1875rem;
+		color: var(--themeBtnColor);
+		transition: var(--transition-3);
+		&:hover {
+			color: var(--themeBtnHoverColor);
 		}
-		img{
+
+		/* svg {
 			width: 20px;
 			height: 20px;
-		}
+			color: var(--themeBtnColor);
+			&:hover {
+				color: var(--themeBtnHoverColor);
+			}
+		} */
 	}
 `
 
