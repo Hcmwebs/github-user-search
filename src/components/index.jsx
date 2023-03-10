d124aa6741ba7844ec9870a30df0e00513b9e173
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-10 14:37:19 +1300

    created, indexed and rendered the theme buttons
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,7 @@
 import Navbar from './Navbar'
 import Search from './Search'
 import Info from './Info'
+import LightBtn from './LightBtn'
+import DarkBtn from './DarkBtn'
 
-export { Navbar, Search, Info }
+export { Navbar, Search, Info, LightBtn, DarkBtn }
