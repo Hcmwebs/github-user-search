1202d9b64273428c1507e197891565ca1d9cd57d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-14 17:40:24 +1300

    created and rendered the custom useContext hook

---
+++ src/context/useGithubContext.jsx
@@ -0,0 +1,35 @@
+import React, {createContext,useContext, useState, useEffect} from 'react'
+
+const GithubContext = createContext()
+
+const GithubProvider = ({children}) =>{
+
+  const getLocalStorageTheme = () => {
+    let theme = 'lightTheme'
+    if (localStorage.getItem('theme')) {
+      theme = localStorage.getItem('theme')
+    }
+    return theme
+  }
+  const [theme, setTheme] = useState(getLocalStorageTheme())
+
+  useEffect(() => {
+		document.documentElement.className = theme
+		localStorage.setItem('theme', theme)
+	}, [theme])
+
+	const toggleTheme = () => {
+		if (theme === 'lightTheme') {
+			setTheme('darkTheme')
+		} else {
+			setTheme('lightTheme')
+		}
+	}
+  return <GithubContext.Provider value = {{toggleTheme,theme,}}>{children}</GithubContext.Provider>
+}
+
+const useGithubContext = () => {
+  return useContext(GithubContext)
+}
+
+export {useGithubContext, GithubProvider}
