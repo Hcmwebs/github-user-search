d725ee2cd584f684f86de44df8cbbf741817a748
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-14 17:40:49 +1300

    rendered the GithubProvider
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { GithubProvider } from './context/useGithubContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<GithubProvider>
+			<App />
+		</GithubProvider>
+	</React.StrictMode>
 )
