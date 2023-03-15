da170c882a97384878aa61180783e90690111e76
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-16 11:51:33 +1300

    added more functions and states to the context
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/context/useGithubContext.jsx
+++ src/context/useGithubContext.jsx
@@ -1,21 +1,26 @@
-import React, {createContext,useContext, useState, useEffect} from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import axios from 'axios'
+import mockUser from '../data/Data'
 const rootUrl = 'https://api.github.com'
 
 const GithubContext = createContext()
 
-const GithubProvider = ({children}) =>{
+const GithubProvider = ({ children }) => {
+	const [githubUser, setGithubUser] = useState(mockUser)
 
-  const getLocalStorageTheme = () => {
-    let theme = 'lightTheme'
-    if (localStorage.getItem('theme')) {
-      theme = localStorage.getItem('theme')
-    }
-    return theme
-  }
-  const [theme, setTheme] = useState(getLocalStorageTheme())
-  
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState({ show: false, msg: '' })
 
-  useEffect(() => {
+	const getLocalStorageTheme = () => {
+		let theme = 'lightTheme'
+		if (localStorage.getItem('theme')) {
+			theme = localStorage.getItem('theme')
+		}
+		return theme
+	}
+	const [theme, setTheme] = useState(getLocalStorageTheme())
+
+	useEffect(() => {
 		document.documentElement.className = theme
 		localStorage.setItem('theme', theme)
 	}, [theme])
@@ -27,11 +32,42 @@ const GithubProvider = ({children}) =>{
 			setTheme('lightTheme')
 		}
 	}
-  return <GithubContext.Provider value = {{toggleTheme,theme,}}>{children}</GithubContext.Provider>
+
+	const searchGithubUser = async (user) => {
+    toggleError()
+		setIsLoading(true)
+		const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+			console.log(err)
+		)
+		if (response) {
+			setGithubUser(response.data)
+		} else {
+			toggleError(true, 'Sorry, No Results!')
+		}
+		setIsLoading(false)
+	}
+
+	const toggleError = (show = false, msg = '') => {
+		setError({ show, msg })
+	}
+
+	return (
+		<GithubContext.Provider
+			value={{
+				toggleTheme,
+				theme,
+				githubUser,
+				error,
+				isLoading,
+				searchGithubUser,
+			}}>
+			{children}
+		</GithubContext.Provider>
+	)
 }
 
 const useGithubContext = () => {
-  return useContext(GithubContext)
+	return useContext(GithubContext)
 }
 
-export {useGithubContext, GithubProvider}
+export { useGithubContext, GithubProvider }
