60652785bb35c71e698a9bbfbd131366401ef373
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-17 14:00:04 +1300

    added a wrapper to the compo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import React from 'react'
+import styled from 'styled-components'
 import { useGithubContext } from '../context/useGithubContext'
 
 const ProfileHeader = () => {
@@ -7,7 +8,7 @@ const ProfileHeader = () => {
 
 	const { avatar_url, name, twitter_username, created_at } = githubUser
 	return (
-		<div className='profileHeader'>
+		<Wrapper className='profileHeader'>
 			<img src={avatar_url} alt={name || 'github user'} />
 			<div className='profileInfo'>
 				<div>
@@ -16,8 +17,58 @@ const ProfileHeader = () => {
 				</div>
 				<small>joined {moment(created_at).format('Do MMM YYYY')}</small>
 			</div>
-		</div>
+		</Wrapper>
 	)
 }
 
+const Wrapper = styled.div`
+	&.profileHeader {
+		display: flex;
+		justify-content: start;
+		align-items: center;
+		gap: 2rem;
+
+		img {
+			width: 70px;
+			height: 70px;
+			border-radius: 50%;
+
+			@media (min-width: 768px) {
+				width: 117px;
+				height: 117px;
+			}
+		}
+		.profileInfo {
+			width: 100%;
+			align-self: flex-start;
+			display: grid;
+			justify-content: start;
+			grid-template-columns: 1fr;
+			gap: 0.375rem;
+			@media (min-width: 1024px) {
+				grid-template-columns: repeat(2, 1fr);
+			}
+			h1 {
+				font-size: 1rem;
+				font-weight: var(--bold);
+				line-height: 1.5rem;
+			}
+			p {
+				font-size: 0.8125rem;
+				font-weight: var(--regular);
+				line-height: 1.1875rem;
+				color: var(--btnBgColor);
+			}
+			small {
+				font-size: 0.8125rem;
+				font-weight: var(--regular);
+				line-height: 1.1875rem;
+				text-transform: capitalize;
+				@media (min-width: 1024px) {
+					justify-self: center;
+				}
+			}
+		}
+	}
+`
 export default ProfileHeader
