e46a9bc66e07a6e77ebd3cc4ca563134e47bd937
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-03-20 12:49:19 +1300

    changed info handle to use login instead of twitter
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -6,14 +6,14 @@ import { useGithubContext } from '../context/useGithubContext'
 const ProfileHeader = () => {
 	const { githubUser } = useGithubContext()
 
-	const { avatar_url, name, twitter_username, created_at } = githubUser
+	const { avatar_url, name, login, created_at } = githubUser
 	return (
 		<Wrapper className='profileHeader'>
 			<img src={avatar_url} alt={name || 'github user'} />
 			<div className='profileInfo'>
 				<div>
 					<h1>{name || 'The Octocat'}</h1>
-					<p>@{twitter_username || 'theoctocat'}</p>
+					<p>@{login || 'theoctocat'}</p>
 				</div>
 				<small>joined {moment(created_at).format('Do MMM YYYY')}</small>
 			</div>
